0bb7d0e1420845a849589261a0f0162b3c687209
Rafaeldev2002 <rafaeldev2002@gmail.com>
2022-06-21 17:18:02 -0300

    add meus exercicios

---
+++ exercicios/aula11/ex006.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`sua velocidade é ${vel}kmph `)
+if (vel > 60) {
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ exercicios/aula11/ex007.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brail') {
+    console.log('Voce é estrangeiro!')
+} else {
+    console.log('voce é brasileiro')
+}
