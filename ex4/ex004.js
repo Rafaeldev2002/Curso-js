e8e273132736187994887e398cb527c9ec7d97a8
Rafaeldev2002 <rafaeldev2002@gmail.com>
2022-06-28 21:41:58 -0300

    Update ex004.js

--- ex4/ex004.js
+++ ex4/ex004.js
@@ -1,14 +1,15 @@
 function calcular() {
-    var numb = document.getElementById = ('txtn')
-    var tab = document.getElementById = ('seltab')
+    var numb = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
     if (numb.value.length == 0 ) {
         window.alert('ensira um valor')
     } else {
         var n = Number(numb.value)
         var c = 1
+        tab.innerHTML = ''
         while (c <= 10) {
             var item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
+            item.text = ` ${n} x ${c} = ${n*c}`
             tab.appendChild(item)
             c++
         }
